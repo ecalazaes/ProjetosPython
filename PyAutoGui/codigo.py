396e3d91240a7cfebd0069cf99e1f2f406711927
Erick Calazães <ecalazaes@gmail.com>
2024-01-09 16:28:07 -0300

    Primeiro commit

---
+++ PyAutoGui/codigo.py
@@ -0,0 +1,9 @@
+# Passo a passo do projeto
+
+# Passo 1 - Entrar no sistema da empresa
+    # https://dlp.hashtagtreinamentos.com/python/intensivao/login    
+# Passo 2 - Fazer login
+# Passo 3 - Importar base de dados
+# Passo 4 - Cadastrar um produtos
+# Passo 5 - Repetir isso até acabar a base de dados
+
